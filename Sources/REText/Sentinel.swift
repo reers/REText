d8b45a92da6b7c151b6065fd07244c4288783eee
Phoenix <x.rhythm@qq.com>
2025-04-03 16:57:58 +0800

    refactor: rewrite Sentinel

--- Sources/REText/Sentinel.swift
+++ Sources/REText/Sentinel.swift
@@ -20,22 +20,85 @@
 //  THE SOFTWARE.
 
 import os.lock
+#if canImport(Synchronization)
+import Synchronization
+#endif
 
-final class Sentinel {
+#if canImport(Synchronization)
+@available(iOS 18.0, macOS 15.0, tvOS 18.0, watchOS 11.0, visionOS 2.0, *)
+private final class AtomicCounterImpl {
+    private let atomicCounter = Atomic<Int64>(0)
+    
+    func increase() -> Int64 {
+        return atomicCounter.add(1, ordering: .relaxed).newValue
+    }
+
+    var value: Int64 {
+        atomicCounter.load(ordering: .relaxed)
+    }
+}
+#endif
+
+private final class LockCounterImpl {
     private var counter: Int64 = 0
     private var lock = os_unfair_lock()
-    
+
     func increase() -> Int64 {
         os_unfair_lock_lock(&lock)
         defer { os_unfair_lock_unlock(&lock) }
-        
         counter += 1
         return counter
     }
-    
+
     var value: Int64 {
         os_unfair_lock_lock(&lock)
         defer { os_unfair_lock_unlock(&lock) }
         return counter
     }
 }
+
+final class Sentinel {
+
+    #if canImport(Synchronization)
+    private let implementation: AnyObject
+    #else
+    private let implementation: LockCounterImpl
+    #endif
+
+    init() {
+        #if canImport(Synchronization)
+        if #available(iOS 18.0, macOS 15.0, tvOS 18.0, watchOS 11.0, visionOS 2.0, *) {
+            self.implementation = AtomicCounterImpl()
+        } else {
+            self.implementation = LockCounterImpl()
+        }
+        #else
+        self.implementation = LockCounterImpl()
+        #endif
+    }
+
+    func increase() -> Int64 {
+        #if canImport(Synchronization)
+        if #available(iOS 18.0, macOS 15.0, tvOS 18.0, watchOS 11.0, visionOS 2.0, *) {
+            return (implementation as! AtomicCounterImpl).increase()
+        } else {
+            return (implementation as! LockCounterImpl).increase()
+        }
+        #else
+        return implementation.increase()
+        #endif
+    }
+
+    var value: Int64 {
+        #if canImport(Synchronization)
+        if #available(iOS 18.0, macOS 15.0, tvOS 18.0, watchOS 11.0, visionOS 2.0, *) {
+            return (implementation as! AtomicCounterImpl).value
+        } else {
+            return (implementation as! LockCounterImpl).value
+        }
+        #else
+        return implementation.value
+        #endif
+    }
+}
+
